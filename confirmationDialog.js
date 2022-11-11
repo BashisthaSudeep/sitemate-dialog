775f3a0724a6f36647929bc17bdc88cbb695dd56
Sudeep Bashistha <sudeep@dzangolab.com>
2022-11-12 00:00:51 +0545

    feat: pass onYes and onCancel params

--- confirmationDialog.js
+++ confirmationDialog.js
@@ -2,7 +2,15 @@ const template = document.createElement('template');
 
 template.innerHTML = `
   <div id="confirmationDialog">
-    <h3>Dialog Goes Here</h3>
+    <div className="overlay">
+      <div className="dialog">
+        <p id="dialogTitle"></p>
+        <div className="dialog-actions">
+          <button id="buttonYes">Yes</button>
+          <button id="buttonCancel">Cancel</button>
+        </div>
+      </div>
+    </div>
   </div>
 `
 
@@ -11,6 +19,10 @@ class ConfirmationDialog extends HTMLElement{
       super();
       this.attachShadow({ mode: 'open'});
       this.shadowRoot.appendChild(template.content.cloneNode(true));
+      this.shadowRoot.getElementById('dialogTitle').innerText = this.getAttribute('message');
+
+      this.shadowRoot.getElementById('buttonYes').addEventListener('click', () => eval(this.getAttribute('onYes'))())
+      this.shadowRoot.getElementById('buttonCancel').addEventListener('click', () => eval(this.getAttribute('onCancel'))())
   }
 
   connectedCallback(){
