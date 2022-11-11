88561bc5d6c60c09df933e342897100b9f32b8d7
Sudeep Bashistha <sudeep@dzangolab.com>
2022-11-12 01:32:41 +0545

    feat: add dialog functionality

--- confirmationDialog.js
+++ confirmationDialog.js
@@ -1,14 +1,35 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <div id="confirmationDialog">
-    <div className="overlay">
-      <div className="dialog">
-        <p id="dialogTitle"></p>
-        <div className="dialog-actions">
-          <button id="buttonYes">Yes</button>
-          <button id="buttonCancel">Cancel</button>
-        </div>
+<style>
+    .overlay {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      height: 100vh;
+      width: 100vw;
+      background-color: rgba(0,0,0,0.3);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      display: none;
+    }
+
+    .overlay.open {
+      display: flex;
+    }
+
+    .dialog {
+      background-color: #FFFFFF;
+      padding: 20px 30px;
+    }
+  </style>
+  <div class="overlay" id="confirmationDialog">
+    <div class="dialog">
+      <p id="dialogTitle"></p>
+      <div class="dialog-actions">
+        <button id="buttonYes">Yes</button>
+        <button id="buttonCancel">Cancel</button>
       </div>
     </div>
   </div>
@@ -20,9 +41,15 @@ class ConfirmationDialog extends HTMLElement{
       this.attachShadow({ mode: 'open'});
       this.shadowRoot.appendChild(template.content.cloneNode(true));
       this.shadowRoot.getElementById('dialogTitle').innerText = this.getAttribute('message');
-
-      this.shadowRoot.getElementById('buttonYes').addEventListener('click', () => eval(this.getAttribute('onYes'))())
-      this.shadowRoot.getElementById('buttonCancel').addEventListener('click', () => eval(this.getAttribute('onCancel'))())
+      this.shadowRoot.getElementById('confirmationDialog').setAttribute('id', this.getAttribute('modalId'))
+      this.shadowRoot.getElementById('buttonYes').addEventListener('click', () => {
+        eval(this.getAttribute('onYes'))()
+        this.shadowRoot.getElementById(this.getAttribute('modalId')).classList.remove("open")
+      })
+      this.shadowRoot.getElementById('buttonCancel').addEventListener('click', () => {
+        eval(this.getAttribute('onCancel'))()
+        this.shadowRoot.getElementById(this.getAttribute('modalId')).classList.remove("open")
+      })
   }
 
   connectedCallback(){
